839a07f3a4125160ab83c29b75dacd7efb5bcf30
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-02-03 21:35:29 -0300

    💄 Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle/>
     </div>
   );
 }
