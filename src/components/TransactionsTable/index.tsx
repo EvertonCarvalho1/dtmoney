b656e38930be37ab5f60d40229c8f3ae27b5576b
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-02-15 19:57:49 -0300

    Listando transações e seeds

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+interface Transaction {
+    id: number;
+    title: String;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: String; 
+}
+
+export function TransactionTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+    //sempre que o estado for um vetor ou um objeto, devemos obrigatóriamente informar um formato pra eles
+
+    useEffect(()=> {
+
+        api.get('transactions')
+        .then((response) => setTransactions(response.data.transactions))
+
+        //Para o Fetch
+        //primeiro .then converte a resposta pra Json
+        //o segundo .then pega os dados convertidos e imprime no console
+
+        //Para o Axios
+        // o proprio axios já converte a resposta em json, então precisaremos apenas de um .then
+        
+    }, []);
+
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {transactions.map(transactions => {
+                        //toda vez que é feito um map, o primeiro elemento desse map deve ter uma key unica
+                        return(
+                            <tr>
+                                <td key={transactions.id}>
+                                    {transactions.title}
+                                </td>
+                                <td className={transactions.type}>
+                                    {transactions.amount}
+                                </td> 
+                                <td>
+                                    {transactions.category}
+                                </td> 
+                                <td>
+                                    {transactions.createdAt}
+                                </td> 
+                            </tr>
+                        )
+                    })}    
+                 
+                </tbody> 
+            </table>
+        </Container>
+    )
+}
